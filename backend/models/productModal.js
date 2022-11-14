10333c3fff2d95982683519bcd8056fd6675c301
Muhammad Kashif <muhammad.kashif72462@gmail.com>
2022-11-14 19:06:16 +0500

    Ecommerce

---
+++ backend/models/productModal.js
@@ -0,0 +1,156 @@
+// import mongoose from "mongoose";
+
+// const productSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: [true, "Please enter product name"],
+//     trim: true,
+//   },
+//   description: {
+//     type: String,
+//     required: [true, "Please enter product Descrioption"],
+//   },
+//   price: {
+//     type: String,
+//     required: [true, "Please enter product Price"],
+//     maxLength: [8, "Price must be less then 8 digit"],
+//   },
+//   ratings: {
+//     type: String,
+//     default: 0,
+//   },
+//   images: [
+//     {
+//       public_id: {
+//         type: String,
+//         required: true,
+//       },
+//       url: {
+//         type: String,
+//         required: true,
+//       },
+//     },
+//   ],
+//   category: {
+//     type: String,
+//     required: [true, "Please Enter Product Category"],
+//   },
+//   stock: {
+//     type: Number,
+//     required: [true, "Please Enter Product Stock"],
+//     maxLength: [4, "Stock can't exceed 4 digit"],
+//     default: 1,
+//   },
+//   numOfReviews: {
+//     type: Number,
+//     default: 0,
+//   },
+//   reviews: [
+//     {
+//       user: {
+//         type: mongoose.Schema.ObjectId,
+//         ref: "User",
+//         required: true,
+//       },
+//       name: {
+//         type: String,
+//         required: true,
+//       },
+//       rating: {
+//         type: Number,
+//         required: true,
+//       },
+//       comment: {
+//         type: String,
+//       },
+
+//     },
+//   ],
+//   user: {
+//     type: mongoose.Schema.ObjectId,
+//     ref: "User",
+//     required: true,
+//   },
+//   createdAt: {
+//     type: Date,
+//     default: Date.now,
+//   },
+// });
+
+// export const Product = mongoose.model("Product", productSchema);
+
+// const mongoose = require("mongoose");
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please Enter product Name"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, "Please Enter product Description"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please Enter product Price"],
+    maxLength: [8, "Price cannot exceed 8 characters"],
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      path: { type: String },
+    },
+  ],
+  category: {
+    type: String,
+    required: [true, "Please Enter Product Category"],
+  },
+  stock: {
+    type: Number,
+    required: [true, "Please Enter product Stock"],
+    maxLength: [4, "Stock cannot exceed 4 characters"],
+    default: 1,
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const Product = mongoose.model("Product", productSchema);
